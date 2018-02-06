78475b887e82afd693760bc5fc829b638d4c5ef4
Unknown <kirisamedesuuu@gmail.com>
2018-02-06 11:22:21 +0700

    Fix PHP and HTML
    -Fix table in show-check.php

--- Pages/Check.php
+++ Pages/Check.php
@@ -18,15 +18,83 @@
                     <h5>ตรวจสอบการจองเวลากิจกรรม</h5>
                 </div>
             </div>
-            <div class="row card">
+            <div class="row">
                 <div class="col-sm-6">
                     <form action="show_check.php" method="Post">
-                        <input name="room">
-                        <input name="day" type="date">
-                        <input type="submit" onclick="w3.removeclass('#open','sleep')">ยืนยัน</a>
+                    <div class="col-sm-6">
+                            <label> ห้องที่จอง </label>
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <label class="custom-control custom-radio">
+                                    <input id="radioStacked3" name="radio-stacked" type="radio" class="custom-control-input" value="1">
+                                    <span class="custom-control-indicator"></span>
+                                    <span class="custom-control-description">ห้องเรียน </span>
+                                </label>
+                            </div>
+                            <div class="col-sm-6">    
+                                <input type="text" name="password" id="inputRoomCode" placeholder="รหัสห้องเรียน 4 ตัว">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <label class="custom-control custom-radio">
+                                    <input id="radioStacked3" name="radio-stacked" type="radio" class="custom-control-input" value="2">
+                                    <span class="custom-control-indicator"></span>
+                                    <span class="custom-control-description">ห้องบรรยายรวม </span>
+                                </label>
+                            </div>
+                        </div>
+					    <div class="row">
+                            <div class="col-sm-12">
+                                <label class="custom-control custom-radio">
+                                    <input id="radioStacked3" name="radio-stacked" type="radio" class="custom-control-input" value="3">
+                                    <span class="custom-control-indicator"></span>
+                                    <span class="custom-control-description">ห้องปฏิบัติการชีววิทยา </span>
+                                </label>
+                            </div>
+                        </div>
+					    <div class="row">
+                            <div class="col-sm-12">
+                                <label class="custom-control custom-radio">
+                                    <input id="radioStacked3" name="radio-stacked" type="radio" class="custom-control-input" value="4">
+                                    <span class="custom-control-indicator"></span>
+                                    <span class="custom-control-description">ห้องปฏิบัติการเคมี </span>
+                                </label>
+                            </div>
+                        </div>
+					    <div class="row">
+                            <div class="col-sm-12">
+                                <label class="custom-control custom-radio">
+                                    <input id="radioStacked3" name="radio-stacked" type="radio" class="custom-control-input" value="5">
+                                    <span class="custom-control-indicator"></span>
+                                    <span class="custom-control-description">ห้องปฏิบัติการฟิสิกส์ </span>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <label class="custom-control custom-radio">
+                                    <input id="radioStacked3" name="radio-stacked" type="radio" class="custom-control-input" value="6">
+                                    <span class="custom-control-indicator"></span>
+                                    <span class="custom-control-description">หอประชุม </span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                        <div class="col-sm-6">
+                            <input name="day" type="date">
+                        </div>
                     </form>
-
+                </div>
             </div>
+            <div class="row col-sm-12 card">
+                <iframe src="show_check.php">
+            </div>
+            <div class="row"><div class="col-sm-6"></div>
+            <div class="col-sm-6 btn-group-vertical">
+                <a class="btn btn-primary btn-block btn-lg" role="button" href="index-login.php">กลับสู่หน้าหลัก</a>
+                <a class="btn btn-block btn-secondary" role="button" href="booking.php">กลับไปจองเพิ่ม</a>
+            </div></div>
         </div>
     </body>
 </html>
--- Pages/add_Booking.php
+++ Pages/add_Booking.php
@@ -13,50 +13,64 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<div class="contianer-fluid">
-			<div class="row">
-				<div class="col-sm-12"><h5>สถานะการจอง</h5></div>
-</div>
-<div class="row"><div class="col-sm-8">
-		<?php
-		$roomrend=$POST['roomrend'];
-		$password=$POST['password'];
-		$user=$_POST['user'];
-		$sub=$_POST['sub'];
-		$grade=$_POST['grade'];
-		$class=$_POST['class'];
-		$day=$_POST['day'];
-		$timein=$_POST['timein'];
-		$timeout=$_POST['timeout'];
-		$count='0';
-		$link=mysqli_connect("127.0.0.1","root");
-		mysqli_set_charset($link,'utf8');
-		mysqli_query($link,"Use projectend;");
-		if($roomrend=='1')
-			$room=$password;
-		else
-			$room=$roomrend;
-		$sql="Select * From roomtable2 Where roomrend == $room and day == $day;";
-		$result=mysqli_query($link,$sql);
-		where($dbarr=mysqli_fetch_array($result))
-		{
-			if($timein>=$dbarr["timein"]&&$timein<=$dbarr["timeout"])
-				$count='1';
-			if($timeout>=$dbarr["timein"]&&$timeout<=$dbarr["timeout"])
-				$count='1';
-		}
-		if($count=='0')
-		{
-			$sql="Insert into roomtable2 Values('$room','$sub','$day','$user','$grade','$class','$timein','$timeout');";
-			$result=mysqli_query($link,$sql);
-			echo "<p>ท่านได้ทำการจองเสร็จสิ้นแล้ว</p>";
-		}
-		else
-			echo "<p>กรุณาไปตรวจสอบข้อมูลใหม่</p>"
-		mysqli_close($link);
-		?>
+		<div class="container-fluid">
+		<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true" id="Warning">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header" style="overflow:hidden; background-color:rgb(255, 238, 0)">
+                            <h5 class="modal-title">สถานะการจอง</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <?php
+							$roomrend=$POST['roomrend'];
+							$password=$POST['password'];
+							$user=$_POST['user'];
+							$sub=$_POST['sub'];
+							$grade=$_POST['grade'];
+							$class=$_POST['class'];
+							$day=$_POST['day'];
+							$timein=$_POST['timein'];
+							$timeout=$_POST['timeout'];
+							$count='0';
+							$link=mysqli_connect("127.0.0.1","root");
+							mysqli_set_charset($link,'utf8');
+							mysqli_query($link,"Use projectend;");
+							if($roomrend=='1')
+								$room=$password;
+							else
+								$room=$roomrend;
+							$sql="Select * From roomtable2 Where roomrend == $room and day == $day;";
+							$result=mysqli_query($link,$sql);
+							where($dbarr=mysqli_fetch_array($result))
+							{
+								if($timein>=$dbarr["timein"]&&$timein<=$dbarr["timeout"])
+									$count='1';
+								if($timeout>=$dbarr["timein"]&&$timeout<=$dbarr["timeout"])
+									$count='1';
+							}
+							?>
+						<p>
+						<?php
+						if($count=='0')
+						{
+							$sql="Insert into roomtable2 Values('$room','$sub','$day','$user','$grade','$class','$timein','$timeout');";
+							$result=mysqli_query($link,$sql);
+							echo "ท่านได้ทำการจองเสร็จสิ้นแล้ว";
+						}
+						else
+							echo "กรุณาไปตรวจสอบข้อมูลใหม่"
+						mysqli_close($link);
+						?></p>
+                        </div>
+                        <div class="modal-footer">
+                            <a role="button" type="summit" class="btn btn-outline-prima" href="index-login.php">รับทราบ</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
 		</div>
-		<div class="col-sm-4"><button type="button" href="index-login.php">รับทราบ</button>
-	</div><div></div>
 	</body>
 </html>
