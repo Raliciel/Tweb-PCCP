a0ccc4b4f7e637500a466494f28442ce18d028c2
Unknown <kirisamedesuuu@gmail.com>
2018-02-07 15:57:04 +0700

    ls
    this
    hell

--- Pages/Check.php
+++ Pages/Check.php
@@ -18,83 +18,82 @@
                     <h5>ตรวจสอบการจองเวลากิจกรรม</h5>
                 </div>
             </div>
-            <div class="row">
+            <div class="row card">
                 <div class="col-sm-6">
-                    <form action="show_check.php" method="Post" target="displayer">
-                    <div class="col-sm-6">
-                            <label> ห้องที่จอง </label>
-                        <div class="row">
-                            <div class="col-sm-4">
-                                <label class="custom-control custom-radio">
-                                    <input id="radioStacked3" name="radio-stacked" type="radio" class="custom-control-input" value="1">
-                                    <span class="custom-control-indicator"></span>
-                                    <span class="custom-control-description">ห้องเรียน </span>
-                                </label>
-                            </div>
-                            <div class="col-sm-6">    
-                                <input type="text" name="password" id="inputRoomCode" placeholder="รหัสห้องเรียน 4 ตัว">
-                            </div>
+                    <form action="show_check.php" method="Post">
+                        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h5>สถานที่นัดจอง</h5>
+                </div>
+                <div class="col-sm-6" id="stra">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <label class="custom-control custom-radio">
+                                <input id="radioStacked3" name="roomrend" type="radio" class="custom-control-input" value="1">
+                                <span class="custom-control-indicator"></span>
+                                <span class="custom-control-description">ห้องเรียน </span>
+                            </label>
                         </div>
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <label class="custom-control custom-radio">
-                                    <input id="radioStacked3" name="radio-stacked" type="radio" class="custom-control-input" value="2">
-                                    <span class="custom-control-indicator"></span>
-                                    <span class="custom-control-description">ห้องบรรยายรวม </span>
-                                </label>
-                            </div>
-                        </div>
-					    <div class="row">
-                            <div class="col-sm-12">
-                                <label class="custom-control custom-radio">
-                                    <input id="radioStacked3" name="radio-stacked" type="radio" class="custom-control-input" value="3">
-                                    <span class="custom-control-indicator"></span>
-                                    <span class="custom-control-description">ห้องปฏิบัติการชีววิทยา </span>
-                                </label>
-                            </div>
-                        </div>
-					    <div class="row">
-                            <div class="col-sm-12">
-                                <label class="custom-control custom-radio">
-                                    <input id="radioStacked3" name="radio-stacked" type="radio" class="custom-control-input" value="4">
-                                    <span class="custom-control-indicator"></span>
-                                    <span class="custom-control-description">ห้องปฏิบัติการเคมี </span>
-                                </label>
-                            </div>
-                        </div>
-					    <div class="row">
-                            <div class="col-sm-12">
-                                <label class="custom-control custom-radio">
-                                    <input id="radioStacked3" name="radio-stacked" type="radio" class="custom-control-input" value="5">
-                                    <span class="custom-control-indicator"></span>
-                                    <span class="custom-control-description">ห้องปฏิบัติการฟิสิกส์ </span>
-                                </label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <label class="custom-control custom-radio">
-                                    <input id="radioStacked3" name="radio-stacked" type="radio" class="custom-control-input" value="6">
-                                    <span class="custom-control-indicator"></span>
-                                    <span class="custom-control-description">หอประชุม </span>
-                                </label>
-                            </div>
+                        <div class="col-sm-6">    
+                            <input type="text" name="password" id="inputRoomCode" placeholder="รหัสห้องเรียน 4 ตัว">
                         </div>
                     </div>
-                        <div class="col-sm-6">
-                            <input name="day" type="date">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <label class="custom-control custom-radio">
+                                    <input id="radioStacked3" name="roomrend" type="radio" class="custom-control-input" value="2">
+                                    <span class="custom-control-indicator"></span>
+                                    <span class="custom-control-description">ห้องบรรยายรวม </span>
+                            </label>
                         </div>
-                    </form>
+                    </div>
+					<div class="row">
+                        <div class="col-sm-12">
+                            <label class="custom-control custom-radio">
+                                    <input id="radioStacked3" name="roomrend" type="radio" class="custom-control-input" value="3">
+                                    <span class="custom-control-indicator"></span>
+                                    <span class="custom-control-description">ห้องปฏิบัติการชีววิทยา </span>
+                            </label>
+                        </div>
+                    </div>
+					<div class="row">
+                        <div class="col-sm-12">
+                            <label class="custom-control custom-radio">
+                                    <input id="radioStacked3" name="roomrend" type="radio" class="custom-control-input" value="4">
+                                    <span class="custom-control-indicator"></span>
+                                    <span class="custom-control-description">ห้องปฏิบัติการเคมี </span>
+                            </label>
+                        </div>
+                    </div>
+					<div class="row">
+                        <div class="col-sm-12">
+                            <label class="custom-control custom-radio">
+                                    <input id="radioStacked3" name="roomrend" type="radio" class="custom-control-input" value="5">
+                                    <span class="custom-control-indicator"></span>
+                                    <span class="custom-control-description">ห้องปฏิบัติการฟิสิกส์ </span>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <label class="custom-control custom-radio">
+                                <input id="radioStacked3" name="roomrend" type="radio" class="custom-control-input" value="6">
+                                <span class="custom-control-indicator"></span>
+                                <span class="custom-control-description">หอประชุม </span>
+                            </label>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="row col-sm-12 card">
-                <iframe src="show_check.php" name="displayer">
-            </div>
-            <div class="row"><div class="col-sm-6"></div>
-            <div class="col-sm-6 btn-group-vertical">
-                <a class="btn btn-primary btn-block btn-lg" role="button" href="index-login.php">กลับสู่หน้าหลัก</a>
-                <a class="btn btn-block btn-secondary" role="button" href="booking.php">กลับไปจองเพิ่ม</a>
-            </div></div>
         </div>
+                        <input name="day" type="date">
+                        <input type="submit" onclick="w3.removeclass('#open','sleep')">ยืนยัน</a>
+                    </form>
+
+            </div>
+        </div>
+		<div class="col-sm-4">
+	</div>
     </body>
 </html>
